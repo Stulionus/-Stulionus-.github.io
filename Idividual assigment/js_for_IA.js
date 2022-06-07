8fc4113f8a40c73b683392b69ab24e865a090076
Stulionus <mcapcanari@gmail.com>
2022-06-07 10:05:52 -0700

    added carousele

---
+++ Idividual assigment/js_for_IA.js
@@ -0,0 +1,28 @@
+document.querySelectorAll(".carousel").forEach(carousel => {
+    const items = carousel.querySelectorAll(".item");
+    const buttonsHTML = Array.from(items, () => {
+        return `<span class="car_button"></span>`;
+    });
+
+    carousel.insertAdjacentHTML("beforeend",`
+        <div class="carousel_nav">
+        ${buttonsHTML.join("")}
+        </div>
+    `)
+
+    const buttons = carousel.querySelectorAll(".car_button");
+
+    buttons.forEach((button, i) =>{
+
+        button.addEventListener("click", () =>{
+            items.forEach(item => item.classList.remove("item_selected"));
+            buttons.forEach(button => button.classList.remove("car_button_active"));
+
+            items[i].classList.add("item_selected");
+            button.classList.add("car_button_active");
+        });
+    });
+
+    items[0].classList.add("item_selected");
+    buttons[0].classList.add("car_button_active");
+}); 
