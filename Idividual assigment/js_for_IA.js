2958eaf98e610827d6d6485f66bcce62554569a4
Stulionus <mcapcanari@gmail.com>
2022-06-07 16:49:50 -0700

    added comments

--- Idividual assigment/js_for_IA.js
+++ Idividual assigment/js_for_IA.js
@@ -1,5 +1,5 @@
 
-
+//Simple function that cycles trough image by adding one to the variable and goes back when done
 var counter = 1;
 setInterval(function(){
     document.getElementById('radio' + counter).checked = true;
