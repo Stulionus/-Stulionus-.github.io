cb285bd9e911f4388a59b4b785dbf182241aaf70
Stulionus <mcapcanari@gmail.com>
2022-11-17 09:53:14 -0800

    1

--- Notes/midterm_part2.java
+++ Notes/midterm_part2.java
@@ -1,5 +1,47 @@
+import java.util.Scanner;
+import java.io.*;
 public class midterm_part2{
+    private static int j;
+
     public static void main(String[] args) {
         
+    Scanner inputStream = null;
+    PrintWriter outputStream = null;
+    int StudentID;
+    int i;
+    boolean keepReading = true;
+    int scoreTotal = 0;
+    int numOfScores = 0;
+    try 
+    {
+        inputStream = 
+                new Scanner(new FileInputStream("quizScore.txt"));
+        outputStream = new PrintWriter (
+                new FileOutputStream("quizResults.txt"));
     }
+
+    catch (FileNotFoundException e)
+
+    {
+        System.out.println(e);
+        System.exit(0);
+    }
+
+    while(inputStream.hasNextInt()){
+        StudentID = inputStream.nextInt();
+        while (keepReading){
+            if (inputStream.nextInt() == 0){
+                keepReading = false;
+                System.exit(0);
+            }
+            scoreTotal = scoreTotal + inputStream.nextInt();
+            ++ numOfScores;
+        }
+        outputStream.print(StudentID + " ");
+        outputStream.print("got a " + (scoreTotal/numOfScores));
+    }
+    System.out.println("Scores Succsefuly calculated!");
+    inputStream.close();
+    outputStream.close();
+}
 }
