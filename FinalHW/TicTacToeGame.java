c7683fa85401a748b819088af0faa8162eb7a006
Stulionus <mcapcanari@gmail.com>
2022-12-07 19:41:43 -0800

    final HW work

--- FinalHW/TicTacToeGame.java
+++ FinalHW/TicTacToeGame.java
@@ -1,26 +1,37 @@
-import java.util.InputMismatchException;
+import java.io.*;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 public class TicTacToeGame{
     public static void main(String[] args) {
-        char userChar = '0'; 
-        System.out.println("Weloce to TicTacToeGame");
+        System.out.println("Your character is: " + getUserCharacter());
+        System.out.println("You are going first " + checkFirstMove());
+    }
+
+    public static char getUserCharacter (){
+        char userChar = '0';
+        System.out.println("Welcome to TicTacToeGame");
         Scanner keyboard = new Scanner(System.in);
         System.out.println("Please choose do you want to play as \"o\" or \"x\".");
+        userChar = keyboard.next().charAt(0);
+
+
         boolean keepAsking = true;
-        while (keepAsking){
-        if (keyboard.next().charAt(0) == 'o' || keyboard.next().charAt(0) == 'O'){
-            keepAsking = false;
-            userChar = 'o';
-        } else if (keyboard.nextInt() == 0){
-            userChar = 'o';
-        }else if (keyboard.next().charAt(0) == 'x' || keyboard.next().charAt(0) == 'X'){ 
-            keepAsking = false;
-            userChar = 'x';
-        }else {
-            System.out.println("Please enter either \"o\" or \"x\".");
+        while (keepAsking) {
+            if (userChar == 'o' || userChar == 'O') {
+                keepAsking = false;
+                userChar = 'o';
+            }
+            else if (userChar == '0') {
+                userChar = 'o';
+            } else if (userChar == 'x' || userChar == 'X') {
+                keepAsking = false;
+                userChar = 'x';
+            } else {
+                System.out.println("Please enter either \"o\" or \"x\".");
+                userChar = keyboard.next().charAt(0);
+            }
         }
-        }
-        System.out.println("Your character is: " + userChar);
+        return userChar;
     }
 
     public static void showGrid(){
@@ -29,5 +40,30 @@ public class TicTacToeGame{
         System.out.println("   |   |   ");
         System.out.println("-----------");
         System.out.println("   |   |   ");
+    }
+
+    public static boolean checkFirstMove() {
+        boolean isPlayerGoingFirst = true;
+        Scanner inputStream = null;
+        String lastWin = "Computer";
+        try {
+            inputStream = new Scanner(new FileInputStream("winLog.txt"));
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        }
+
+        if (inputStream == null) {
+            return isPlayerGoingFirst;
+        } else {
+            while(inputStream.hasNextLine()){
+                lastWin = inputStream.nextLine();
+            }
+            if (lastWin.equals("Computer")) {
+                return isPlayerGoingFirst;
+            } else if (lastWin.equals("Player")){
+                isPlayerGoingFirst = false;
+            }
+        }
+        return isPlayerGoingFirst;
     }
 }
